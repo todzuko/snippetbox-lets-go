bedde29c122d2f658875678ec6af64e1e4e5de4c
todzuko <todzuko@gmail.com>
2024-08-19 20:15:40 +0200

    sql connection and usage

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"github.com/todzuko/snippetbox-lets-go/internal/models"
+	//"html/template"
 	"net/http"
 	"strconv"
 )
@@ -10,22 +12,32 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	files := []string{
-		"./ui/html/base.tmpl",
-		"ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
-
-	err = ts.ExecuteTemplate(w, "base", nil)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
+	//files := []string{
+	//	"./ui/html/base.tmpl",
+	//	"ui/html/partials/nav.tmpl",
+	//	"./ui/html/pages/home.tmpl",
+	//	"./ui/html/pages/view.tmpl",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//	return
+	//}
+	//
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -35,8 +47,18 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(models.ErrNoRecord, err) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
 
-	fmt.Fprintf(w, "Snippet %d", id)
+	fmt.Fprintf(w, "%+v", snippet)
+
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -44,6 +66,15 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "Save a new snippet")
+	title := "test title"
+	content := "test content"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/todzuko/snippetbox-lets-go
 
 go 1.22.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect

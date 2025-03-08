f805d738f3c0e8ac05ab8b00e755a7d295326dc3
todzuko <todzuko@gmail.com>
2025-03-08 20:13:29 +0100

    snippet create

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,9 +4,17 @@ import (
 	"errors"
 	"fmt"
 	"github.com/todzuko/snippetbox-lets-go/internal/models"
+	"github.com/todzuko/snippetbox-lets-go/internal/validator"
 	"net/http"
 	"strconv"
 )
+
+type snippetCreateForm struct {
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
+}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
@@ -42,15 +50,46 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Snippet created")
+	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
+
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "test title"
-	content := "test content"
-	expires := 7
 
-	id, err := app.snippets.Insert(title, content, expires)
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+	}
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form := snippetCreateForm{
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
+	}
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "The field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "The field must be less than 100 characters")
+	form.CheckField(validator.NotBlank(form.Content), "content", "The field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusOK, "create.tmpl", data)
+		return
+	}
+
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return

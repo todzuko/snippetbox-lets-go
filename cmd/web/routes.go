55f54d816e69c034d06e774022a74179a6e258ef
todzuko <todzuko@gmail.com>
2025-05-09 16:58:37 +0200

    e2e endpoint test and test helper

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,8 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
+	mux.HandleFunc("GET /ping", ping)
+
 	dynamic := alice.New(app.sessionManager.LoadAndSave, app.noSurf, app.authenticate)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))

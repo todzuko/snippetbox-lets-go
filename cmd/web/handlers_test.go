409a7877e136c8572e202b105ea5730ec902aa39
todzuko <todzuko@gmail.com>
2025-05-13 21:55:01 +0200

    html form testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/todzuko/snippetbox-lets-go/internal/assert"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
@@ -70,6 +71,121 @@ func TestSnippetView(t *testing.T) {
 
 			if tt.expectedBody != "" {
 				assert.StringContains(t, body, tt.expectedBody)
+			}
+		})
+	}
+}
+
+func TestUserSignup(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	_, _, body := ts.get(t, "/user/signup")
+	validCsrfToken := extractCSRFToken(t, body)
+
+	const (
+		validName     = "User"
+		validEmail    = "user@example.com"
+		validPassword = "password"
+		formTag       = "<form action='/user/signup' method='POST' novalidate>"
+	)
+
+	tests := []struct {
+		name            string
+		userName        string
+		userEmail       string
+		userPassword    string
+		csrfToken       string
+		expectedCode    int
+		expectedFormTag string
+	}{
+		{
+			name:         "Valid name",
+			userName:     validName,
+			userEmail:    validEmail,
+			userPassword: validPassword,
+			csrfToken:    validCsrfToken,
+			expectedCode: http.StatusSeeOther,
+		},
+		{
+			name:         "Invalid CSRF Token",
+			userName:     validName,
+			userEmail:    validEmail,
+			userPassword: validPassword,
+			csrfToken:    "invalidToken",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:            "Empty username",
+			userName:        "",
+			userEmail:       validEmail,
+			userPassword:    validPassword,
+			csrfToken:       validCsrfToken,
+			expectedCode:    http.StatusUnprocessableEntity,
+			expectedFormTag: formTag,
+		},
+		{
+			name:            "Empty email",
+			userName:        validName,
+			userEmail:       "",
+			userPassword:    validPassword,
+			csrfToken:       validCsrfToken,
+			expectedCode:    http.StatusUnprocessableEntity,
+			expectedFormTag: formTag,
+		},
+		{
+			name:            "Invalid email",
+			userName:        validName,
+			userEmail:       "emiail@test.",
+			userPassword:    validPassword,
+			csrfToken:       validCsrfToken,
+			expectedCode:    http.StatusUnprocessableEntity,
+			expectedFormTag: formTag,
+		},
+		{
+			name:            "Empty password",
+			userName:        validName,
+			userEmail:       validEmail,
+			userPassword:    "",
+			csrfToken:       validCsrfToken,
+			expectedCode:    http.StatusUnprocessableEntity,
+			expectedFormTag: formTag,
+		},
+		{
+			name:            "Short password",
+			userName:        validName,
+			userEmail:       validEmail,
+			userPassword:    "pass",
+			csrfToken:       validCsrfToken,
+			expectedCode:    http.StatusUnprocessableEntity,
+			expectedFormTag: formTag,
+		},
+		{
+			name:            "Duplicate email",
+			userName:        validName,
+			userEmail:       "dupe@test.com",
+			userPassword:    validPassword,
+			csrfToken:       validCsrfToken,
+			expectedCode:    http.StatusUnprocessableEntity,
+			expectedFormTag: formTag,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			form := url.Values{}
+			form.Add("name", tt.userName)
+			form.Add("email", tt.userEmail)
+			form.Add("password", tt.userPassword)
+			form.Add("csrf_token", tt.csrfToken)
+
+			code, _, body := ts.postForm(t, "/user/signup", form)
+
+			assert.Equal(t, code, tt.expectedCode)
+
+			if tt.expectedFormTag != "" {
+				assert.StringContains(t, body, tt.expectedFormTag)
 			}
 		})
 	}
--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,14 +5,28 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	"github.com/todzuko/snippetbox-lets-go/internal/models/mocks"
+	"html"
 	"io"
 	"log/slog"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 )
+
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(matches[1])
+}
 
 func newTestApplication(t *testing.T) *application {
 	templateCache, err := newTemplateCache()
@@ -59,6 +73,22 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
 	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
 	if err != nil {
 		t.Fatal(err)
 	}
